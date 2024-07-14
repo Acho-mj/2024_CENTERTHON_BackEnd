48948e62fe70bfa212ccc0ef63c2f29e0d5a9722
yushinc <cys74351@gmail.com>
2024-07-14 20:05:42 +0900

    first commit

---
+++ src/main/java/likelion12th/centerthon/CenterthonApplication.java
@@ -0,0 +1,13 @@
+package likelion12th.centerthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterthonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion12th/centerthon/CenterthonApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion12th.centerthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterthonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
